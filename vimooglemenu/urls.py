10b2576e079da7bfc017054afc8ac7d7485a8e18
VimoogleMenu <vimooglemenu@geomarcomputers.com>
2012-03-17 18:48:17 -0700

    Modified vimooglemenu.haml to be on on a sub url of main site, cleanup project urls.py - site.com/whatever/link0/ instead of site.com/link0/ - test current link via request.pathinfo instead of request.path - make current link identification for login and help buttons links changed from /link0/ to ../link0/ Project level urls: disabled admin, no handling of static files, forward everything to homepage/urls.py

--- vimooglemenu/urls.py
+++ vimooglemenu/urls.py
@@ -1,19 +1,25 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-
+##admin module disabled
+##Uncomment the next two lines to enable the admin:
+#from django.contrib import admin
+#admin.autodiscover()
 
 
 #urlpatterns = patterns('django.views.static', 
 #    url(r'^js/(?P<path>.*)$',       'serve', {'document_root': '/home/picturep/sites/djangovimooglemenu/vimooglemenu/static/js/'    ,  'show_indexes': True} ),
 #    url(r'^css/(?P<path>.*)$',      'serve', {'document_root': '/home/picturep/sites/djangovimooglemenu/vimooglemenu/static/css/'   ,  'show_indexes': True} ),
 #    url(r'^images/(?P<path>.*)$',   'serve', {'document_root': '/home/picturep/sites/djangovimooglemenu/vimooglemenu/static/images/',  'show_indexes': True} ),
+#    url(r'^js/(?P<path>.*)$',       'serve', {'document_root': '/home/user/python/djangovimooglemenu/vimooglemenu/homepage/static/js/'    ,  'show_indexes': True} ),
+#    url(r'^css/(?P<path>.*)$',      'serve', {'document_root': '/home/user/python/djangovimooglemenu/vimooglemenu/homepage/static/css/'   ,  'show_indexes': True} ),
+#    url(r'^images/(?P<path>.*)$',   'serve', {'document_root': '/home/user/python/djangovimooglemenu/vimooglemenu/homepage/static/images/',  'show_indexes': True} ),
+#    url(r'^js/(?P<path>.*)$',       'serve', {'document_root': settings.CSS_ROOT} ),
+#    url(r'^css/(?P<path>.*)$',      'serve', {'document_root': settings.JS_ROOT} ),
+#    url(r'^images/(?P<path>.*)$',   'serve', {'document_root': settings.IMAGES_ROOT} ),
 #)
 
 
-#from django#.conf import settings
+#from django.conf import settings
 #if settings.DEBUG:
 #    urlpatterns += patterns('django.views.static',
 #        url(r'^media/(?P<path>.*)$','serve', {'document_root': settings.MEDIA_ROOT,}  ),
@@ -22,21 +28,17 @@ admin.autodiscover()
 
 
 #urlpatterns += patterns('',
-urlpatterns = patterns('',
-    # Example:
-    # (r'^project/', include('project.foo.urls')),
+urlpatterns   = patterns('',
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    #(r'^admin/', include(admin.site.urls)),
+#   #admin module disabled
+#   #Uncomment the admin/doc line below and add 'django.contrib.admindocs' to INSTALLED_APPS to enable admin documentation:
+#   (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+#   #Uncomment the next line to enable the admin:
+#   (r'^admin/', include(admin.site.urls)),
     
     (r'', include('homepage.urls')),
-    
-    
 )
+
 
 
 
